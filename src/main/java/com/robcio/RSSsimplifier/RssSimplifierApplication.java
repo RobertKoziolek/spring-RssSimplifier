d5b9300138f52bca1f536a2fd011972778c35fe7
Robert Koziolek <robertkoziolek4c@gmail.com>
2018-11-20 09:23:46 +0100

    first commit

---
+++ src/main/java/com/robcio/RSSsimplifier/RssSimplifierApplication.java
@@ -0,0 +1,12 @@
+package com.robcio.RSSsimplifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssSimplifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssSimplifierApplication.class, args);
+	}
+}
